4ef4f47b4c855ebadc5ec8e2b796d54205ac011c
Zac Smith <zrs3141592@gmail.com>
2020-05-29 19:38:56 -0600

    added BSTNode and improved names.py
    Current results are 64 duplications in about ~0.16 seconds

---
+++ names/binary_search_tree.py
@@ -0,0 +1,38 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # print('contains', self.value)
+        if target == self.value:
+            return True
+
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        elif target >= self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        else:
+            return False
--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -13,16 +14,32 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# start with BSTBode names_1 0 element (first)
+bst_return = BSTNode(names_1[0])
+
+for name in names_1:
+    bst_return.insert(name)
+
+for name in names_2:
+    if bst_return.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+# ------------------ BENCH MARK ---------------------
+# Using program without modifications
+# 64 Duplicates
+#Time[~9.4, ~11.1, ~10.3]
