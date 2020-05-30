4d89f6fadc5b0ca16afa314e2bc4b10075a22815
Zac Smith <zrs3141592@gmail.com>
2020-05-29 21:21:21 -0600

    ring buffer passing all test, MVP earned

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,28 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            if not self.current.next:
+                self.current.value = item
+                self.current = self.storage.head
+            else:
+                self.current.value = item
+                self.current = self.current.next
 
     def get(self):
-        pass
+        node_arr = []
+        current = self.storage.head
+        while current:
+            node_arr.append(current.value)
+            current = current.next
+        return node_arr
